8280afe58b6938740a8a61833bc67c2e9454a048
OverDriveGameStudios <115117808+bennnnn-hue@users.noreply.github.com>
2025-03-17 17:15:37 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,151 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image5":
+            case "myImage4":return img`
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 f f 1 1 1 1 1 1 f f 1 1 1 
+1 1 1 f f f 1 1 1 1 f f f 1 1 1 
+1 1 1 1 f f 1 1 1 1 f f 1 1 1 1 
+1 1 1 1 f f 1 1 1 1 f f 1 1 1 1 
+1 1 1 1 f f 1 1 1 1 f f 1 1 1 1 
+1 1 1 1 f f 1 1 1 1 f f 1 1 1 1 
+1 1 1 1 f f f 1 1 f f f 1 1 1 1 
+1 1 1 1 1 f f 1 1 f f 1 1 1 1 1 
+1 1 1 1 1 f f 1 1 f f 1 1 1 1 1 
+1 1 1 1 1 f f 1 1 f f 1 1 1 1 1 
+1 1 1 1 1 f f f f f f 1 1 1 1 1 
+1 1 1 1 1 1 f f f f 1 1 1 1 1 1 
+1 1 1 1 1 1 1 f f 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+`;
+            case "image4":
+            case "myImage3":return img`
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 f f f f 1 f f 1 1 1 1 1 f f 1 
+1 f f f f 1 f f f 1 1 1 f f f 1 
+1 1 f f 1 1 1 f f 1 1 1 f f 1 1 
+1 1 f f 1 1 1 f f 1 1 1 f f 1 1 
+1 1 f f 1 1 1 f f 1 1 1 f f 1 1 
+1 1 f f 1 1 1 f f 1 1 1 f f 1 1 
+1 1 f f 1 1 1 f f f 1 f f f 1 1 
+1 1 f f 1 1 1 1 f f 1 f f 1 1 1 
+1 1 f f 1 1 1 1 f f 1 f f 1 1 1 
+1 1 f f 1 1 1 1 f f 1 f f 1 1 1 
+1 f f f f 1 1 1 f f f f f 1 1 1 
+1 f f f f 1 1 1 1 f f f 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+`;
+            case "image2":
+            case "myImage2":return img`
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 f f f f 1 f f f f 1 f f f f 1 
+1 f f f f 1 f f f f 1 f f f f 1 
+1 1 f f 1 1 1 f f 1 1 1 f f 1 1 
+1 1 f f 1 1 1 f f 1 1 1 f f 1 1 
+1 1 f f 1 1 1 f f 1 1 1 f f 1 1 
+1 1 f f 1 1 1 f f 1 1 1 f f 1 1 
+1 1 f f 1 1 1 f f 1 1 1 f f 1 1 
+1 1 f f 1 1 1 f f 1 1 1 f f 1 1 
+1 1 f f 1 1 1 f f 1 1 1 f f 1 1 
+1 1 f f 1 1 1 f f 1 1 1 f f 1 1 
+1 f f f f 1 f f f f 1 f f f f 1 
+1 f f f f 1 f f f f 1 f f f f 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+`;
+            case "image1":
+            case "myImage0":return img`
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 f f f f 1 1 1 1 f f f f 1 1 
+1 1 f f f f 1 1 1 1 f f f f 1 1 
+1 1 1 f f 1 1 1 1 1 1 f f 1 1 1 
+1 1 1 f f 1 1 1 1 1 1 f f 1 1 1 
+1 1 1 f f 1 1 1 1 1 1 f f 1 1 1 
+1 1 1 f f 1 1 1 1 1 1 f f 1 1 1 
+1 1 1 f f 1 1 1 1 1 1 f f 1 1 1 
+1 1 1 f f 1 1 1 1 1 1 f f 1 1 1 
+1 1 1 f f 1 1 1 1 1 1 f f 1 1 1 
+1 1 1 f f 1 1 1 1 1 1 f f 1 1 1 
+1 1 f f f f 1 1 1 1 f f f f 1 1 
+1 1 f f f f 1 1 1 1 f f f f 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+`;
+            case "image3":
+            case "myImage1":return img`
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 f f f f 1 1 1 1 1 1 
+1 1 1 1 1 1 f f f f 1 1 1 1 1 1 
+1 1 1 1 1 1 1 f f 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 f f 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 f f 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 f f 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 f f 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 f f 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 f f 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 f f 1 1 1 1 1 1 1 
+1 1 1 1 1 1 f f f f 1 1 1 1 1 1 
+1 1 1 1 1 1 f f f f 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim":
+            case "anim1":return [img`
+. . . . . . . . . 
+. . . . . . . . . 
+. . . 1 1 1 . . . 
+. . 1 1 d 1 1 . . 
+. . 1 d d d 1 . . 
+. . 1 1 d 1 1 . . 
+. . . 1 1 1 . . . 
+. . . . . . . . . 
+. . . . . . . . . 
+`, img`
+. . . . . . . . . 
+. . 1 1 1 1 1 . . 
+. 1 . . . . . 1 . 
+. 1 . 1 d 1 . 1 . 
+. 1 . d d d . 1 . 
+. 1 . 1 d 1 . 1 . 
+. 1 . . . . . 1 . 
+. . 1 1 1 1 1 . . 
+. . . . . . . . . 
+`, img`
+. . 1 1 1 1 1 . . 
+. 1 . . . . . 1 . 
+1 . . . . . . . 1 
+1 . . 1 d 1 . . 1 
+1 . . d d d . . 1 
+1 . . 1 d 1 . . 1 
+1 . . . . . . . 1 
+. 1 . . . . . 1 . 
+. . 1 1 1 1 1 . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
